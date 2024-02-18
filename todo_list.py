4bdb31d57fd766454e21750983a416ca49bba333
Rand-Sai <yuan.sa@northeastern.edu>
2024-02-17 22:47:14 -0500

    Create TO DO LIST program file

---
+++ todo_list.py
@@ -0,0 +1,10 @@
+#Project Task: Write a Python program that simulates a simple "To-Do List". The program should allow users to:
+# Add new tasks.
+# View all tasks.
+# Mark tasks as completed.
+# Delete tasks.
+
+def main():
+    print("Select action: Add Task | View Task | Mark as Complete | Delete Task \n")
+
+main()
